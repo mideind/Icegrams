2e259106a9b9980d3e7c08d5db44ad14335c5848
Vilhjalmur Thorsteinsson <vt@extrada.com>
2019-03-08 17:39:07 +0000

    Added .sh files for release mechanism

---
+++ build_wheels.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Build the icegrams wheels on the CentOS5 base manylinux1 platform
+# This script should be executed inside the Docker container!
+# It is invoked indirectly from wheels.sh
+
+# Stop execution upon error; show executed commands
+set -e -x
+
+# Compile wheels for Python 3.4-3.7
+for PYBIN in cp34 cp35 cp36 cp37
+do
+#    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "/opt/python/${PYBIN}-${PYBIN}m/bin/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/icegrams-*.whl
+do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
+# Set read/write permissions on the wheels
+chmod 666 /io/wheelhouse/*
---
+++ release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Build an Icegrams release and upload it to PyPi
+if [ "$1" = "" ]; then
+   echo "Version name argument missing"
+   exit 1
+fi
+echo "Upload a new Icegrams version:" "$1"
+# Fix permission bits
+chmod -x src/icegrams/*.py
+chmod -x src/icegrams/*.cpp
+chmod -x src/icegrams/*.h
+chmod -x src/icegrams/resources/*
+# Create the base source distribution
+python setup.py sdist
+# Create the binary wheels
+source wheels.sh
+# Upload the new release
+twine upload dist/icegrams-$1*
+echo "Upload of" "$1" "done"
