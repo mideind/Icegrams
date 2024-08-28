60ee25dcde50214a645226a6dd297e0029472505
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2024-08-28 17:58:43 +0000

    CI wheels config

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     package_data={"icegrams": ["py.typed"]},
     include_package_data=True,
     zip_safe=False,
-    setup_requires=["cffi>=1.15.1"],
-    install_requires=["cffi>=1.15.1"],
+    setup_requires=["cffi>=1.15.1", "setuptools"],
+    install_requires=["cffi>=1.15.1", "setuptools"],
     cffi_modules=["src/icegrams/trie_build.py:ffibuilder"],
 )
